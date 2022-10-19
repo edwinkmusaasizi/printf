4df9f6bdb88a0852faa4a584bd721bad0f878d7d
steelfreak <kinenkey@gmail.com>
2022-10-19 12:56:01 -0700

    Re-did modifiers.c

--- modifiers.c
+++ modifiers.c
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-nclude "main.h"
+#include "main.h"
 
 unsigned int print_width(buffer_t *output, unsigned int printed,
 		unsigned char flags, int wid);
@@ -7,16 +6,6 @@ unsigned int print_string_width(buffer_t *output,
 		unsigned char flags, int wid, int prec, int size);
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
 		unsigned char flags, int wid);
-=======
-#include "main.h"
-
-unsigned int print_width(buffer_t *output, unsigned int printed,
-			 unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-				unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-			     unsigned char flags, int wid);
->>>>>>> kinen
 
 /**
  * print_width - Stores leading spaces to a buffer for a width modifier.
@@ -29,7 +18,6 @@ unsigned int print_neg_width(buffer_t *output, unsigned int printed,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_width(buffer_t *output, unsigned int printed,
-<<<<<<< HEAD
 		unsigned char flags, int wid)
 {
 	unsigned int ret = 0;
@@ -42,20 +30,6 @@ unsigned int print_width(buffer_t *output, unsigned int printed,
 	}
 
 	return (ret);
-=======
-			 unsigned char flags, int wid)
-{
-  unsigned int ret = 0;
-  char width = ' ';
-
-  if (NEG_FLAG == 0)
-    {
-      for (wid -= printed; wid > 0;)
-	ret += _memcpy(output, &width, 1);
-    }
-
-  return (ret);
->>>>>>> kinen
 }
 
 /**
@@ -69,7 +43,6 @@ unsigned int print_width(buffer_t *output, unsigned int printed,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_string_width(buffer_t *output,
-<<<<<<< HEAD
 		unsigned char flags, int wid, int prec, int size)
 {
 	unsigned int ret = 0;
@@ -83,21 +56,6 @@ unsigned int print_string_width(buffer_t *output,
 	}
 
 	return (ret);
-=======
-				unsigned char flags, int wid, int prec, int size)
-{
-  unsigned int ret = 0;
-  char width = ' ';
-
-  if (NEG_FLAG == 0)
-    {
-      wid -= (prec == -1) ? size : prec;
-      for (; wid > 0; wid--)
-	ret += _memcpy(output, &width, 1);
-    }
-
-  return (ret);
->>>>>>> kinen
 }
 
 /**
@@ -111,7 +69,6 @@ unsigned int print_string_width(buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-<<<<<<< HEAD
 		unsigned char flags, int wid)
 {
 	unsigned int ret = 0;
@@ -124,18 +81,4 @@ unsigned int print_neg_width(buffer_t *output, unsigned int printed,
 	}
 
 	return (ret);
-=======
-			     unsigned char flags, int wid)
-{
-  unsigned int ret = 0;
-  char width = ' ';
-
-  if (NEG_FLAG == 1)
-    {
-      for (wid -= printed; wid > 0; wid--)
-	ret += _memcpy(output, &width, 1);
-    }
-
-  return (ret);
->>>>>>> kinen
 }
